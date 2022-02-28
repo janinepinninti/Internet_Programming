458ab4f442825ad88e9a670c009241d08460136e
janinepinninti <janineamurao@ymail.com>
2022-02-28 21:19:10 +0000

    3rd commit Week4 Exercise4

---
+++ Week4 Exercise/test4.js
@@ -0,0 +1,9 @@
+function myLoadFunction() {
+    var element1 = document.getElementById ('pageheading');
+    element1.firstChild.nodeValue = 'This is the New Heading';
+
+    var element2 = document.getElementById ('pagecontent');
+    element2.firstChild.nodeValue = 'This is the New Content';
+} 
+
+document.addEventListener ('DOMContentLoaded', myLoadFunction);
