de3a6ae86679f5cb109f21aeb065db7d62882891
janinepinninti <janineamurao@ymail.com>
2022-03-07 16:21:51 +0000

    5th commit Week5 Exercise5

--- Week5 Exercise/script5.js
+++ Week5 Exercise/script5.js
@@ -1,12 +1,12 @@
 function myClickFunction() {
     var element = document.getElementById ('circle');
     var circleOpacity = parseFloat (element.style.opacity);
-    element.style.opacity = circleOpacity + 0.1;
+    element.style.opacity = circleOpacity - 0.1;
 }
 function myLoadFunction() {
     var element = document.getElementById ('circle');
     element.addEventListener('click', myClickFunction);
-    element.style.opacity = 0.5;
+    element.style.opacity = 1;
 } 
 
 document.addEventListener ('DOMContentLoaded', myLoadFunction);
