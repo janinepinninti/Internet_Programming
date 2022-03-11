8f3663dd0b8bfd214860664a887a4654dfd03445
janinepinninti <janineamurao@ymail.com>
2022-03-11 06:19:58 +0000

    7th commit Week5 Exercise7&8

---
+++ Week5 Exercise/script7.js
@@ -0,0 +1,12 @@
+    function myKeyDown(){
+        var element = document.getElementById ('circle');
+        var positionLeft = element.offsetLeft;
+        element.style.left = positionLeft - 10 + 'px';
+    }
+
+    function myLoadFunction() {
+        document.addEventListener('keydown', myKeyDown);
+    } 
+    
+    document.addEventListener ('DOMContentLoaded', myLoadFunction);
+    
