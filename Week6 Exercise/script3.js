1d02d5f27315b2c490740c1ab503e778ad7eb507
janinepinninti <janineamurao@ymail.com>
2022-03-16 23:05:25 +0000

    4th commit for Week6 Exercise4

---
+++ Week6 Exercise/script3.js
@@ -0,0 +1,40 @@
+function myLoadFunction(){
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Asia', 'Africa', 'Europe', 'North America', 'South America', 'Australia/Oceania','Antartica'],
+            datasets: [{
+                label: 'Population per Continent',
+                data: [4641054775, 1340598147, 747636026, 592072212, 430759766, 43111704, 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    }
+    
+    document.addEventListener('DOMContentLoaded', myLoadFunction);
